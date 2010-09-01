4c730bec69a26d2cfb049ec64f5cd3dad63bafa3
Marcus Thiesen <marcus@thiesen.org>
2010-09-01 20:01:21 +0200

    make annotations usage more clear

--- src/org/thiesen/helenaorm/annotations/HelenaBean.java
+++ src/org/thiesen/helenaorm/annotations/HelenaBean.java
@@ -25,10 +25,13 @@
  */
 package org.thiesen.helenaorm.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface HelenaBean {
     String keyspace();
     String columnFamily();
--- src/org/thiesen/helenaorm/annotations/KeyProperty.java
+++ src/org/thiesen/helenaorm/annotations/KeyProperty.java
@@ -25,10 +25,13 @@
  */
 package org.thiesen.helenaorm.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.METHOD } )
 public @interface KeyProperty {
     // annotation
 }
